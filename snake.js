6fc04905300107f474a7d88589534ea239f315a5
Anatoly Maremianin <maremianin@gmail.com>
2024-02-01 20:16:20 +0300

    code redesigned

--- snake.js
+++ snake.js
@@ -1,231 +1,300 @@
-const canvas = document.getElementById('canvas');
-// Get the device pixel ratio, falling back to 1.
-const dpr = window.devicePixelRatio || 1;
-// Get the size of the canvas in CSS pixels.
-let rect = canvas.getBoundingClientRect();
-// Give the canvas pixel dimensions of their CSS
-// size * the device pixel ratio.
-canvas.width = rect.width * dpr;
-canvas.height = rect.height * dpr;
-const window_width = window.innerWidth;
-const window_height = window.innerHeight;
-const ctx = canvas.getContext('2d');
-ctx.scale(dpr, dpr);
-///////////// consts
-const sound_chew = new Audio("chew.wav");
-const background_color = 'rgb(0, 0, 0)';
-const snake_cell_color = 'rgb(0, 255, 0)';
-const cherry_cell_color = 'rgb(255, 0, 0)';
-const max_cherry_num = 1;
-const game_delay = 3;
-const grid_rows = 20,
-    grid_cols = 20;
-const max_tail_size = grid_rows * grid_cols + 1;
-const cell_height = window_height / grid_rows,
-    cell_width = window_width / grid_cols;
-const cell_size = cell_height < cell_width ? cell_height : cell_width;
-const cell_color = 'rgba(255, 255, 255, 0.1)';
-const snake_spawn_point = [grid_cols / 2, grid_rows / 2];
-//////////////////////////////////////////////////////////
-let index = 0;
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
+
+// game starter (audio issues otherwise)
+const button_play = document.getElementById("button_play");
+button_play.addEventListener("click", start_canvas, false);
+
+const snake_chew_sound = new Audio("chew.wav");
+snake_chew_sound.preload = "auto";
+const background_color = "rgba(0, 0, 0, 1)";
+const snake_cell_color = "rgba(0, 255, 0, 1)";
+const cherry_cell_color = "rgba(255, 0, 0, 1)";
+const cell_color = "rgba(255, 255, 255, 0.75)";
+const cherry_max_n = 1;
+const n_cols = 20,
+  n_rows = 20;
+// potential max tail size in respect to grid dimensions
+const global_snake_tail_max_size = n_rows * n_cols;
+const cell_size = {
+  width: canvas.width / n_cols,
+  height: canvas.height / n_rows,
+};
+const snake_spawn_point = {
+  x: n_cols / 2,
+  y: n_rows / 2,
+};
 
 function gen_int(min, max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min) + min); //The maximum is exclusive and the minimum is inclusive
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  // [min, max)
+  return Math.floor(Math.random() * (max - min) + min);
 }
 
 class Cherry {
-    constructor(x, y, cell_color) {
-        this.x = x;
-        this.y = y;
-        this.cell_color = cell_color;
-    }
-    draw(ctx, cell_size) {
-        ctx.fillStyle = this.cell_color;
-        ctx.fillRect(this.x * cell_size, this.y * cell_size, cell_size, cell_size);
-    }
+  constructor(x, y, fill_color) {
+    this.x = x;
+    this.y = y;
+    this.fill_color = fill_color;
+  }
+  draw(ctx) {
+    ctx.fillStyle = this.fill_color;
+    ctx.fillRect(
+      this.x * cell_size.width,
+      this.y * cell_size.height,
+      cell_size.width,
+      cell_size.height
+    );
+  }
 }
 
 class Snake {
-    constructor(x, y, max_tail_size, cell_color, sound_chew) {
-        this.start_x = x;
-        this.start_y = y;
-        this.x = x;
-        this.y = y;
-        this.vx = 1;
-        this.vy = 0;
-        this.tail = [
-            [this.x, this.y]
-        ];
-        this.tail_size = 1;
-        this.max_tail_size = max_tail_size;
-        this.cell_color = cell_color;
-        this.sound_chew = sound_chew;
+  constructor(x, y, fill_color) {
+    this.x = x;
+    this.y = y;
+    this.speed_x = 1;
+    this.speed_y = 0;
+    // queue
+    this.tail = [];
+    //
+    this.tail_map = new Map();
+    this.tail_max_size = 1;
+    this.fill_color = fill_color;
+    this.sound = snake_chew_sound;
+  }
+  draw(ctx) {
+    ctx.fillStyle = this.fill_color;
+    for (let i = 0; i < this.tail.length; ++i) {
+      ctx.fillRect(
+        this.tail[i].x * cell_size.width,
+        this.tail[i].y * cell_size.height,
+        cell_size.width,
+        cell_size.height
+      );
     }
-    move(grid_rows, grid_cols) {
-        this.x += this.vx;
-        this.y += this.vy;
-        if (this.x >= grid_cols) {
-            this.x = 0;
-        }
-        if (this.x < 0) {
-            this.x = grid_cols - 1;
-        }
-        if (this.y >= grid_rows) {
-            this.y = 0;
-        }
-        if (this.y < 0) {
-            this.y = grid_rows - 1;
-        }
-        this.tail.push([this.x, this.y]);
-        if (this.tail.length === this.tail_size + 1) {
-            this.tail.shift();
-        }
-        for (let i = this.tail.length - 2; i > -1; --i) {
-            if (this.x === this.tail[i][0] &&
-                this.y === this.tail[i][1]) {
-                this.#restart();
-                break;
-            }
-        }
+    ctx.fillRect(
+      this.x * cell_size.width,
+      this.y * cell_size.height,
+      cell_size.width,
+      cell_size.height
+    );
+  }
+  move() {
+    // tail processing
+    if (this.tail.length === this.tail_max_size) {
+      let removed_el = this.tail.shift();
+      this.tail_map.delete(`(${removed_el.x},${removed_el.y})`);
     }
-    draw(ctx, cell_size) {
-        ctx.fillStyle = this.cell_color;
-        for (let i = 0; i < this.tail.length; ++i) {
-            ctx.fillRect(this.tail[i][0] * cell_size, this.tail[i][1] * cell_size, cell_size, cell_size);
-        }
+    this.tail.push({ x: this.x, y: this.y });
+    this.tail_map.set(`(${this.x},${this.y})`, { x: this.x, y: this.y });
+
+    // move processing
+    this.x += this.speed_x;
+    this.y += this.speed_y;
+    if (this.x >= n_cols) {
+      this.x = 0;
+    } else if (this.x < 0) {
+      this.x = n_cols - 1;
     }
-    chew() {
-        this.sound_chew.play();
+    if (this.y >= n_rows) {
+      this.y = 0;
+    } else if (this.y < 0) {
+      this.y = n_rows - 1;
     }
-    #restart() {
-        this.x = this.start_x;
-        this.y = this.start_y;
-        this.vx = 1;
-        this.vy = 0;
-        this.tail = [];
-        this.tail_size = 1;
-    }
+  }
+  // play eat sound (chew)
+  play_sound() {
+    this.sound.play();
+  }
 }
 
+const Game = {
+  IN_PROGRESS: true,
+  END: false,
+};
 class Game_Field {
-    constructor(rows, cols, cell_size, cell_color, snake, max_cherry_num, cherry_cell_color) {
-        this.rows = rows;
-        this.cols = cols;
-        this.cell_size = cell_size;
-        this.cell_color = cell_color;
-        this.snake = snake;
-        this.cherries = [];
-        this.max_cherry_num = max_cherry_num;
-        this.cherry_cell_color = cherry_cell_color;
+  constructor(snake, rows, cols, stroke_color) {
+    this.snake = snake;
+    this.rows = rows;
+    this.cols = cols;
+    this.stroke_color = stroke_color;
+    this.cherries = new Map();
+    this.status = Game.IN_PROGRESS;
+  }
+  draw(ctx) {
+    ctx.strokeStyle = this.stroke_color;
+    for (let i = 0; i < this.cols; ++i) {
+      for (let j = 0; j < this.rows; ++j) {
+        ctx.strokeRect(
+          i * cell_size.width,
+          j * cell_size.height,
+          cell_size.width,
+          cell_size.height
+        );
+      }
     }
-    move_snake() {
-        this.snake.move(this.rows, this.cols);
+    for (let cherry of this.cherries.values()) {
+      cherry.draw(ctx);
     }
-    spawn_cherry() {
-        for (let i = 0; i < this.max_cherry_num - this.cherries.length; ++i) {
-            if (this.cherries.length > this.rows * this.cols - this.snake.tail.length) {
-                break;
-            }
-            let temp_x = 0;
-            let temp_y = 0;
-            let pass = false;
-            while (!pass) {
-                pass = true;
-                temp_x = gen_int(0, this.cols);
-                temp_y = gen_int(0, this.rows);
-                for (let j = 0; j < this.snake.tail.length; ++j) {
-                    if (temp_x === this.snake.tail[j][0] && temp_y === this.snake.tail[j][1]) {
-                        pass = false;
-                        break;
-                    }
-                }
-                if (!pass) {
-                    continue;
-                }
-                for (let j = 0; j < this.cherries.length; ++j) {
-                    if (temp_x === this.cherries[j].x && temp_y === this.cherries[j].y) {
-                        pass = false;
-                        break;
-                    }
-                }
-            }
-            this.cherries.push(new Cherry(temp_x, temp_y, this.cherry_cell_color));
-        }
+    this.snake.draw(ctx);
+  }
+  spawn_cherry() {
+    for (let i = 0; i < cherry_max_n - this.cherries.size; ++i) {
+      //check if any space left to spawn a new cherry
+      if (
+        this.rows * this.cols - this.cherries.size - this.snake.tail.length <=
+        0
+      ) {
+        break;
+      }
+
+      let x = gen_int(0, this.cols);
+      let y = gen_int(0, this.rows);
+      while (
+        this.snake.tail_map.has(`(${x},${y})`) ||
+        this.cherries.has(`(${x},${y})`)
+      ) {
+        x = gen_int(0, this.cols);
+        y = gen_int(0, this.rows);
+      }
+      let cherry = new Cherry(x, y, cherry_cell_color);
+      this.cherries.set(`(${cherry.x},${cherry.y})`, cherry);
     }
-    check_cherry() {
-        let remove_index = 0;
-        for (let i = 0; i < this.cherries.length - remove_index; ++i) {
-            if (this.snake.x === this.cherries[i].x && this.snake.y === this.cherries[i].y) {
-                this.snake.chew();
-                if (this.snake.tail_size !== this.snake.max_tail_size) {
-                    this.snake.tail_size += 1;
-                }
-                this.cherries.splice(i - remove_index, 1);
-                ++remove_index;
-            }
-        }
+  }
+  // check snake's head collision with cherry (yumm)
+  check_cherry() {
+    if (this.cherries.has(`(${this.snake.x},${this.snake.y})`)) {
+      this.cherries.delete(`(${this.snake.x},${this.snake.y})`);
+      this.snake.play_sound();
+      ++this.snake.tail_max_size;
     }
-    draw(ctx) {
-        ctx.strokeStyle = this.cell_color;
-        for (let i = 0; i < this.rows; ++i) {
-            for (let j = 0; j < this.cols; ++j) {
-                ctx.strokeRect(j * this.cell_size, i * cell_size, cell_size, cell_size);
-            }
-        }
-        this.snake.draw(ctx, this.cell_size);
-        for (let i = 0; i < this.cherries.length; ++i) {
-            this.cherries[i].draw(ctx, this.cell_size);
-        }
+  }
+  // check snake's head collision with its tail
+  check_snake() {
+    if (this.snake.tail_map.has(`(${this.snake.x},${this.snake.y})`)) {
+      this.reset();
+      console.log("TRY AGAIN NEXT TIME!");
     }
+  }
+  reset() {
+    // reset snake
+    this.snake.x = snake_spawn_point.x;
+    this.snake.y = snake_spawn_point.y;
+    this.snake.speed_x = 1;
+    this.snake.speed_y = 0;
+    this.snake.tail = [];
+    this.snake.tail_map = new Map();
+    this.snake.tail_max_size = 1;
+    // reset cherries
+    this.cherries = new Map();
+    // reset field
+    this.status = Game.IN_PROGRESS;
+  }
+  // game loop update
+  update() {
+    if (this.snake.tail_size === global_snake_tail_max_size) {
+      end_game();
+      return;
+    }
+    this.check_snake();
+    this.check_cherry();
+    this.spawn_cherry();
+    this.snake.move();
+  }
+  end_game() {
+    // TODO: CODE FOR END GAME CONGRATULATIONS
+    this.status = Game.END;
+    console.log("CONGRATULATIONS!!!");
+  }
 }
 
-document.addEventListener('keydown', (event) => {
+document.addEventListener(
+  "keydown",
+  (event) => {
     const key_name = event.key;
 
-    if (key_name === 'ArrowLeft') {
-        if (snake.vx !== 1) {
-            snake.vx = -1;
-            snake.vy = 0;
-        }
-        return;
-    } else if (key_name === 'ArrowRight') {
-        if (snake.vx !== -1) {
-            snake.vx = 1;
-            snake.vy = 0;
-        }
-        return;
-    } else if (key_name === 'ArrowUp') {
-        if (snake.vy !== 1) {
-            snake.vx = 0;
-            snake.vy = -1;
-        }
-        return;
-    } else if (key_name === 'ArrowDown') {
-        if (snake.vy !== -1) {
-            snake.vx = 0;
-            snake.vy = 1;
-        }
-        return;
+    if (key_name === "ArrowLeft" && field.snake.speed_x !== 1) {
+      field.snake.speed_x = -1;
+      field.snake.speed_y = 0;
+    } else if (key_name === "ArrowRight" && field.snake.speed_x !== -1) {
+      field.snake.speed_x = 1;
+      field.snake.speed_y = 0;
+    } else if (key_name === "ArrowUp" && field.snake.speed_y !== 1) {
+      field.snake.speed_x = 0;
+      field.snake.speed_y = -1;
+    } else if (key_name === "ArrowDown" && field.snake.speed_y !== -1) {
+      field.snake.speed_x = 0;
+      field.snake.speed_y = 1;
     }
-}, false);
+  },
+  false
+);
 
-let snake = new Snake(snake_spawn_point[0], snake_spawn_point[1], max_tail_size, snake_cell_color, sound_chew);
-let field = new Game_Field(grid_rows, grid_cols, cell_size, cell_color, snake, max_cherry_num, cherry_cell_color);
-
-
+// main game update loop
 function update() {
-    ctx.fillStyle = background_color;
-    ctx.fillRect(0, 0, window_width, window_height);
-    field.draw(ctx);
-    if (index > game_delay) {
-        field.move_snake();
-        field.check_cherry();
-        field.spawn_cherry();
-        index = 0;
-    }
-    ++index;
+  if (field.status === Game.END) field.reset();
+
+  time_now = window.performance.now();
+  const time_dur = time_now - time_prev;
+
+  // limit fps
+  if (time_dur < time_per_frame) return;
+
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  ctx.fillStyle = background_color;
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
+  field.draw(ctx);
+  // limit snake movement
+  if (count === game_update_limit) {
+    field.update();
+    count = 0;
+  } else ++count;
+
+  const time_excess = time_dur % time_per_frame;
+  time_prev = time_now - time_excess;
+
+  ++frames;
+
+  window.requestAnimationFrame(update);
+}
+
+function start_canvas() {
+  button_play.style.display = "none";
+  canvas.style.display = "inline";
+  //setInterval(debug_info, 1000);
+  setInterval(check_window_activity, 250);
+}
+
+async function check_window_activity() {
+  if (window.performance.now() - time_prev > time_per_frame * 2)
     requestAnimationFrame(update);
 }
-requestAnimationFrame(update);
+
+async function debug_info() {
+  console.log(
+    `[DEBUG]:\n\tLOOP:\n\t\tFPS: ${frames}\n\tFIELD:\n\t\tN_COLS: ${n_cols}\n\t\tN_ROWS: ${n_rows}\n\t\tCELL_SIZE: ${cell_size.width} ${cell_size.height}\n\t\tCHERRY_MAX_N: ${cherry_max_n}\n\t\tSTATUS: ${field.status}\n\tSNAKE:\n\t\tSNAKE_POS: ${field.snake.x} ${field.snake.y}\n\t\tSNAKE_SPEED: ${field.snake.speed_x} ${field.snake.speed_y}\n\t\tSNAKE_TAIL_SIZE: ${field.snake.tail.length}`
+  );
+  frames = 0;
+}
+
+// main
+
+// game vars init
+let snake = new Snake(
+  snake_spawn_point.x,
+  snake_spawn_point.y,
+  snake_cell_color
+);
+let field = new Game_Field(snake, n_rows, n_cols, cell_color);
+
+// game update loop vars init
+let count = 0;
+const game_update_limit = 3;
+const fps = 60;
+const time_per_frame = 1000 / fps;
+let time_prev = window.performance.now();
+let time_now = undefined;
+
+// debug
+let frames = 0;
